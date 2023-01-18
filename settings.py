a75ba47dfc811201807083408db9e7617be541f9
Shayan Raza <shayanraza07@gmail.com>
2023-01-18 23:30:07 +0500

    reading saved JSON

--- settings.py
+++ settings.py
@@ -6,3 +6,10 @@ def create_json() :
         address = input("Server Address: ")
         json_dic = {"address" : address} #Creating JSON
         json.dump(json_dic, f) #Writing to settings.JSON
+
+#Reading the saved JSON
+def read_json() : 
+    with open("settings.json", "r") as f : #Opening settings.json file
+        address = json.load(f) #Loading JSON file
+        address = address["address"] #Retrieving the address from the JSON
+        return address 
