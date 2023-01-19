9ee2d9bedc99128d20ab4961de2289ab2d5ae472
Shayan Raza <shayanraza07@gmail.com>
2023-01-19 10:36:44 +0500

    indented JSON

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ def create_json() :
     with open("settings.json", "w") as f : 
         address = input("Server Address: ")
         json_dic = {"address" : address} #Creating JSON
-        json.dump(json_dic, f) #Writing to settings.JSON
+        json.dump(json_dic, f, indent=4) #Writing to settings.JSON
 
 #Reading the saved JSON
 def read_json() : 
