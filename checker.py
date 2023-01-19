36076f5c5beca6fdeb4876521e9c5cf9470473b8
Shayan Raza <shayanraza07@gmail.com>
2023-01-19 20:06:10 +0500

    checker.py: removed JSON variable

--- checker.py
+++ checker.py
@@ -1,8 +1,4 @@
-from settings import read_json
 from ping3 import ping
-
-#Reading JSON for address 
-address = read_json() #JSON function from settings.py
 
 #Ping server 
 def ping_server(address) : 
