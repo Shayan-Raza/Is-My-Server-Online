8de9aa672cab3167232d0fae30a465de97848129
Shayan Raza <shayanraza07@gmail.com>
2023-01-19 16:01:13 +0500

    checker.py pinging given server address

--- checker.py
+++ checker.py
@@ -1,4 +1,10 @@
 from settings import read_json
+from ping3 import ping
 
 #Reading JSON for address 
-read_json() #JSON function from settings.py
+address = read_json() #JSON function from settings.py
+
+#Ping server 
+def ping_server(address) : 
+    result = ping(address)
+    return result
