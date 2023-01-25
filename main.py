9071dbbcab669c32676aaaaae30f925b877b1349
Shayan Raza <shayanraza07@gmail.com>
2023-01-25 22:14:02 +0500

    main.py: pinging server and notifying

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from checker import ping_server
 
 # Libraries 
 import os 
+import time
 
 # Creating JSON file
 # Skipping if settings.json already exists 
@@ -14,3 +15,18 @@ else:
     create_json()
 
 address = read_json() #Reading server address from JSON above
+
+#Pinging server
+while True : 
+    if ping_server(address=address) == "Failed to ping server" :
+        print("""
+An error occured:
+Pls check if the script is running in sudo or has admin privileges
+        """)
+        break
+    elif str(ping_server(address=address)) == "False" : 
+        notify(address=address)
+        break
+    else : 
+        print(f"Delay to server {ping_server(address=address)}")
+        time.sleep(10)
